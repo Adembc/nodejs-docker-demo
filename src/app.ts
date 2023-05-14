a5352f3b98f3b67312421eac9e28f7e130128894
Adem Baccara <adembc020@gmail.com>
2023-05-14 15:38:23 +0100

    inital nodejs app

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static("./public"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile("./");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
